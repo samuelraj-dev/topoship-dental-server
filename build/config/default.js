5acb550edb0ec0837f1ecfd27d2b117f8b46dca9
samuelraj-dev <samuelraj.dev@outlook.com>
2024-02-03 13:39:08 +0530

    more few fixes

--- build/config/default.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    logLevel: 'info',
-    saltWorkFactor: 10,
-    accessTokenTtl: "1y",
-    refreshTokenTtl: "1y",
-};
-//# sourceMappingURL=default.js.map
--- build/src/api/domain/medicine/medicine.schema.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=medicine.schema.js.map
