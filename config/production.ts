7edc94e98297a09a61edfa68f758ac61cb281cd4
samuelraj-dev <samuelraj.dev@outlook.com>
2024-02-04 19:31:35 +0530

    test

--- config/production.ts
+++ config/production.ts
@@ -1,7 +1,7 @@
 export default {
     port: 1337,
     // origin: 'http://localhost:5173',
-    origin: 'http://localhost:5173',
+    origin: 'https://dental.topoship.com',
     logLevel: 'info',
     saltWorkFactor: 10,
     accessTokenTtl: "1y",
